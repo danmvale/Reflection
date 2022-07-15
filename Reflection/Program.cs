c7cccc77b89c178c45e9d69d99d60a9a03771e87
DanielMendes <dan.mvale@gmail.com>
2022-07-15 16:26:59 -0300

    final commit

---
+++ Reflection/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace ReflectionEx
+{
+    internal class Program
+    {
+        static List<PropertyInfo> studentPublicProperties = null;
+
+        static void Main(string[] args)
+        {
+            DisplayPublicProperties();
+            CreateInstance();
+        }
+
+        static void DisplayPublicProperties()
+        {
+            studentPublicProperties = typeof(Student)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance).ToList();
+
+            Console.WriteLine("Propriedades públicas da classe Student:\n");
+
+            foreach (var property in studentPublicProperties.Select(x => $"{x.Name} ({x.PropertyType})"))
+                Console.WriteLine(property);
+        }
+
+        static void CreateInstance()
+        {
+            var student = (Student)Activator.CreateInstance(typeof(Student));
+
+            studentPublicProperties.Where(x => x.Name == "Name").First().SetValue(student, "Daniel");
+            studentPublicProperties.Where(x => x.Name == "University").First().SetValue(student, "UFABC");
+            studentPublicProperties.Where(x => x.Name == "RollNumber").First().SetValue(student, 123456);
+
+            Console.WriteLine();
+            typeof(Student).GetMethods().Where(x => x.Name == "DisplayInfo").First().Invoke(student, null);
+        }
+    }
+}
